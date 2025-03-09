8bec9cb2cca237bd08e0353c35cf8786cf8b2b77
Aryan Negi <negiaryan2803@gmail.com>
2025-03-09 15:17:14 +0530

    Resolved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -12,44 +12,45 @@
 
 
 ActiveRecord::Schema[8.0].define(version: 2025_03_09_060948) do
-ActiveRecord::Schema[8.0].define(version: 2025_03_09_064444) do
+  ActiveRecord::Schema[8.0].define(version: 2025_03_09_064444) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+    # These are extensions that must be enabled in order to support this database
+    enable_extension "pg_catalog.plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.string "author"
-    t.decimal "mrp"
-    t.decimal "discounted_price"
-    t.integer "quantity"
-    t.text "book_details"
-    t.string "genre"
-    t.string "book_image"
-    t.boolean "is_deleted", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    create_table "books", force: :cascade do |t|
+      t.string "name"
+      t.string "author"
+      t.decimal "mrp"
+      t.decimal "discounted_price"
+      t.integer "quantity"
+      t.text "book_details"
+      t.string "genre"
+      t.string "book_image"
+      t.boolean "is_deleted", default: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+
+    create_table "users", force: :cascade do |t|
+      t.string "name"
+      t.string "email"
+      t.string "password_digest"
+      t.string "mobile_number"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+
+    create_table "wishlists", force: :cascade do |t|
+      t.bigint "user_id", null: false
+      t.bigint "book_id", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.boolean "is_deleted", default: false
+      t.index ["book_id"], name: "index_wishlists_on_book_id"
+      t.index ["user_id"], name: "index_wishlists_on_user_id"
+    end
+
+    add_foreign_key "wishlists", "books"
+    add_foreign_key "wishlists", "users"
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "mobile_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "wishlists", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "book_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "is_deleted", default: false
-    t.index ["book_id"], name: "index_wishlists_on_book_id"
-    t.index ["user_id"], name: "index_wishlists_on_user_id"
-  end
-
-  add_foreign_key "wishlists", "books"
-  add_foreign_key "wishlists", "users"
 end
