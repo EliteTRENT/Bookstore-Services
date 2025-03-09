842766798a72988f56d2f970d6c531b44a63ac3a
Aryan Negi <negiaryan2803@gmail.com>
2025-03-09 15:39:38 +0530

    [Aryan] MOD: Modified swagger file

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,5 +1,9 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  validates :book_id, uniqueness: { scope: :user_id, message: "is already in your wishlist" }
+  validates :book_id, uniqueness: {
+    scope: :user_id,
+    conditions: -> { where(is_deleted: false) },
+    message: "is already in your wishlist"
+  }
 end
